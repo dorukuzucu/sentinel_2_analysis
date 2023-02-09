891c8e4deb39a6ebcb4e4616e512f3af6fdd2618
Doruk Beren Kuzucu <dorukuzucu@gmail.com>
2023-02-09 17:08:23 +0100

    Image analysis and its tests are added

---
+++ raster_analysis_service/image/analysis.py
@@ -0,0 +1,87 @@
+import ctypes
+import enum
+import logging
+
+from abc import ABC, abstractmethod
+from multiprocessing import Value
+from typing import Any, Dict, Union
+
+import numpy as np
+from .types import RasterImage
+
+
+LOGGER = logging.getLogger("Analysis")
+
+
+class Analysis(ABC):
+    """Base class to use for analysis on raster data
+    """
+    @abstractmethod
+    def add(self, raster_image: RasterImage) -> None:
+        """Adds a new raster image to current set
+        Later, analysis will be calculated over these set
+
+        Args:
+            raster_image (RasterImage): image to analyze
+        """
+
+    @abstractmethod
+    def result(self):
+        """Returns result of an analysis
+        """
+
+
+class MeanValueAnalysis(Analysis):
+    """A class to calculate running mean of all pixel values within an image
+    """
+    pixel_count: Value
+    average_mean: Value
+
+    def __init__(self) -> None:
+        self.pixel_count = Value(ctypes.c_double, 0.0)
+        self.average_mean = Value(ctypes.c_double, 0.0)
+
+    def add(self, raster_image: RasterImage) -> None:
+        image_array = raster_image.array() 
+
+        image_pixel_count = np.prod(image_array.shape)
+        image_sum = image_array.sum()
+
+        self.average_mean.value = (self.average_mean.value * self.pixel_count.value + image_sum) / (self.pixel_count.value + image_pixel_count)
+        self.pixel_count.value += image_pixel_count
+
+    def result(self):
+        return self.average_mean.value
+
+
+class AnalysisType(enum.Enum):
+    """A list of available analysis types"""
+    MEAN_VALUE = "MEAN_VALUE"
+
+
+_ANALYSIS_TYPE_TO_CLASS: Dict[AnalysisType, Any] = {
+    AnalysisType.MEAN_VALUE: MeanValueAnalysis
+}
+
+
+def get_analysis(analysis_type: Union[str, AnalysisType]):
+    """Retrieves analysis class based on given type
+
+    Args:
+        analysis_type (Union[str, AnalysisType]): Name or enum of analysis
+
+    Raises:
+        ValueError: If an invalid type is passed, value error is raised
+
+    Returns:
+        class: An Analysis class
+    """
+    if isinstance(analysis_type, str):
+        try:
+            analysis_type = AnalysisType[analysis_type]
+        except KeyError as error:
+            message = f"Invalid analysis type: {error}"
+            LOGGER.error(message)
+            raise ValueError(message) from error
+    return _ANALYSIS_TYPE_TO_CLASS[analysis_type]
+ 
---
+++ tests/unit/image/test_analysis.py
@@ -0,0 +1,55 @@
+import numpy as np
+import unittest
+
+from typing import Optional
+from unittest.mock import Mock
+
+from raster_analysis_service.image.analysis import Analysis, AnalysisType, get_analysis, MeanValueAnalysis
+
+
+class MeanValueAnalysisTest(unittest.TestCase):
+    analysis: Optional[MeanValueAnalysis]
+
+    def setUp(self) -> None:
+        self.analysis = MeanValueAnalysis()
+
+    def tearDown(self) -> None:
+        self.analysis = None
+
+    def test_initial_values(self):
+        self.assertEqual(self.analysis.pixel_count.value, 0)
+        self.assertEqual(self.analysis.average_mean.value, 0)
+
+    def test_add(self):
+        mock_raster = Mock()
+        mock_raster.array.return_value = np.ones((3, 5, 5))
+        self.analysis.add(mock_raster)
+
+        self.assertEqual(self.analysis.pixel_count.value, 75)
+        self.assertEqual(self.analysis.average_mean.value, 1)
+
+    def test_add_two_images(self):
+        mock_raster = Mock()
+        mock_raster.array.return_value = np.ones((3, 5, 5))
+        self.analysis.add(mock_raster)
+        mock_raster.array.return_value = np.ones((3, 5, 5)) * 5
+        self.analysis.add(mock_raster)
+
+        self.assertEqual(self.analysis.pixel_count.value, 150)
+        self.assertEqual(self.analysis.average_mean.value, 3)
+
+
+class GetAnalysisTest(unittest.TestCase):
+    def test_success_with_enum(self):
+        analysis = get_analysis(AnalysisType.MEAN_VALUE)
+
+        self.assertEqual(analysis, MeanValueAnalysis)
+
+    def test_success_with_string(self):
+        analysis = get_analysis(AnalysisType.MEAN_VALUE.value)
+
+        self.assertEqual(analysis, MeanValueAnalysis)
+
+    def test_invalid_value_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            analysis = get_analysis("ERROR")
